b764111ff6f3f153336dbe510def9be3a51ef4f5
yusufstawan <yusufsetiyawan45@gmail.com>
2022-04-02 23:34:34 +0700

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
+
+import { COLORS, NFTData } from '../constants';
+
+const Home = () => {
+  return (
+    <Text>Home</Text>
+  )
+}
+
+export default Home;
