f77717f08b2feb7c0f8cf5b443976302364ca99e
yusufstawan <yusufsetiyawan45@gmail.com>
2022-04-04 08:59:42 +0700

    NFTcard done dan navigate to detail button

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -1,49 +1,115 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { SIZES, FONTS, COLORS, SHADOWS, assets } from '../constants';
 
-export const NFTTitle = () => {
+export const NFTTitle = ({ title, subTitle, titleSize, subTitleSize }) => {
   return (
     <View>
-      <Text>NFTTitle</Text>
+      <Text style={{
+        fontFamily: FONTS.semiBold,
+        fontSize: titleSize,
+        color: COLORS.primary,
+      }}>
+        {title}
+      </Text>
+      <Text style={{
+        fontFamily: FONTS.regular,
+        fontSize: subTitleSize,
+        color: COLORS.primary,
+      }}>
+        {subTitle}
+      </Text>
     </View>
   )
 }
 
-export const EthPrice = () => {
+export const EthPrice = ({ price }) => {
   return (
-    <View>
-      <Text>EthPrice</Text>
-    </View>
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <Image
+        source={assets.eth}
+        resizeMode="contain"
+        style={{ width: 20, height: 20, marginRight: 2 }}
+      />
+      <Text style={{
+        fontFamily: FONTS.medium,
+        fontSize: SIZES.font,
+        color: COLORS.primary,
+      }}>
+        {price}
+      </Text>
+    </View >
   )
 }
 
-export const ImageCmp = () => {
+export const ImageCmp = ({ imgUrl, index }) => {
   return (
-    <View>
-      <Text>ImageCmp</Text>
-    </View>
+    <Image
+      source={imgUrl}
+      resizeMode="contain"
+      style={{
+        width: 48,
+        height: 48,
+        marginLeft: index === 0 ? 0 : -SIZES.font
+      }}
+    />
   )
 }
 
 export const People = () => {
   return (
-    <View>
-      <Text>People</Text>
+    <View style={{
+      flexDirection: 'row',
+    }}>
+      {[assets.person02, assets.person03, assets.person04].map((imgUrl, index) => (
+        <ImageCmp imgUrl={imgUrl} index={index} key={`People-${index}`} />
+      ))}
     </View>
   )
 }
 
 export const EndDate = () => {
   return (
-    <View>
-      <Text>EndDate</Text>
+    <View
+      style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor: COLORS.white,
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: '50%',
+      }}
+    >
+      <Text style={{
+        fontFamily: FONTS.regular,
+        fontSize: SIZES.small,
+        color: COLORS.primary,
+      }}>
+        Ending in
+      </Text>
+      <Text style={{
+        fontFamily: FONTS.semiBold,
+        fontSize: SIZES.medium,
+        color: COLORS.primary,
+      }}>
+        12h 30m
+      </Text>
     </View>
   )
 }
 
 export const SubInfo = () => {
   return (
-    <View>
-      <Text>SubInfo</Text>
+    <View style={{
+      width: '100%',
+      paddingHorizontal: SIZES.font,
+      marginTop: -SIZES.extraLarge,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    }}>
+      <People />
+      <EndDate />
     </View>
   )
 }
